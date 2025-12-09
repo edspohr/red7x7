ccd3a030c099c859de0c8685ab1054d808d77fbd
edspohr <edmundo@spohr.cl>
2025-12-09 00:13:12 -0300

    bug fixes

--- src/js/data.js
+++ src/js/data.js
@@ -29,7 +29,7 @@ export const addAnnouncement = async (text, isPinned = false, author = "Admin") 
         author,
         date: new Date().toISOString()
     });
-    });
+
 };
 
 export const deleteAnnouncement = async (id) => {
